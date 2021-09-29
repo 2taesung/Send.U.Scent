85d7d18112ebfab3020dfff696ccf63542c139b4
이태성 <dlxotjde@gmail.com>
2021-09-30 03:27:25 +0900

    Add feat/BE/CORS

--- backend/node_mysql/app.js
+++ backend/node_mysql/app.js
@@ -17,6 +17,12 @@ var connection = mysql.createConnection({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 var users = [
  { id: 1, name: "유저1" },
  { id: 2, name: "유저2" },
